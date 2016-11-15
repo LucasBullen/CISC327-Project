ecb4f7f74d3660e5ffcb277adc3dc903ed496ca2
Daniel2218 <daniel_lucia22@hotmail.com>
2016-11-15 14:11:44 -0500

    Added to Transaction.java

--- backEnd/Main.java
+++ backEnd/Main.java
@@ -1,3 +1,12 @@
 private class Main {
 
+    public static void main(String[] args) {
+        Transaction transaction = new Transaction("CR 11111111 00000000 000 GRANT");
+        
+        System.out.println(getType());
+        System.out.println(getSourceAccount());
+        System.out.println(getTargetAccount());
+        System.out.println(getAmount());
+        System.out.println(getSourceName());
+    }
 }
--- backEnd/Transaction.java
+++ backEnd/Transaction.java
@@ -1,12 +1,36 @@
 public class Transaction {
-
     private String type;
     private String sourceAccount;
     private String targetAccount;
-    private String sourceName;
     private Integer amount;
+    private String sourceName;
 
     public Transaction(String data) {
+        String[] tokens = data.split(" ");
+        this.type = tokens[0];
+        this.sourceAccount = token[1];
+        this.targetAccount = token[2];
+        this.amount = Integer.parseInt(token[3]);
+        this.sourceName = token[4];
+    }
 
+    public String getType() {
+        return this.type;
+    }
+
+    public String getSourceAccount() {
+        return this.sourceAccount;
+    }
+
+    public String getTargetAccount() {
+        return this.targetAccount;
+    }
+
+    public Integer getAmount() {
+        return this.amount;
+    }
+
+    public String getSourceName() {
+        return this.sourceName;
     }
 }
