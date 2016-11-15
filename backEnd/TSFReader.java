1d3b72d6887756761d930e5245f9095f9d9027e8
Daniel2218 <daniel_lucia22@hotmail.com>
2016-11-15 13:45:29 -0500

    Created classes

---
+++ backEnd/TSFReader.java
@@ -0,0 +1,10 @@
+public class TSFReader {
+
+    private String name;
+
+    public TSFReader(String name) {
+        this.name = name;
+    }
+
+    
+}
---
+++ backEnd/Transaction.java
@@ -0,0 +1,12 @@
+public class Transaction {
+
+    private String type;
+    private String sourceAccount;
+    private String targetAccount;
+    private String sourceName;
+    private Integer amount;
+
+    public Transaction(String data) {
+
+    }
+}
