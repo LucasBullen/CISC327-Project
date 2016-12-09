2f3be2f719b048dc1291928ed9b8fac71589d6aa
LucasBullen <bullenlucas@gmail.com>
2016-12-08 20:28:00 -0500

    can have better TSF names

--- Daily.sh
+++ Daily.sh
@@ -12,47 +12,47 @@ mv ${VAL}.txt frontEnd/VAL.txt
 
 cd frontEnd
 echo quit >> ../${TS1}
-java Main VAL TSF1 < ../${TS1}
+java Main VAL holdTSF1 < ../${TS1}
 sed '$d' < ../${TS1} > hold ; mv hold ../${TS1}
 
 echo quit >> ../${TS2}
-java Main VAL TSF2 < ../${TS2}
+java Main VAL holdTSF2 < ../${TS2}
 sed '$d' < ../${TS2} > hold ; mv hold ../${TS2}
 
 echo quit >> ../${TS3}
-java Main VAL TSF3 < ../${TS3}
+java Main VAL holdTSF3 < ../${TS3}
 sed '$d' < ../${TS3} > hold ; mv hold ../${TS3}
 
 cd ../
 
-mv frontEnd/TSF1.txt TSF1.txt
-mv frontEnd/TSF2.txt TSF2.txt
-mv frontEnd/TSF3.txt TSF3.txt
+mv frontEnd/holdTSF1.txt holdTSF1.txt
+mv frontEnd/holdTSF2.txt holdTSF2.txt
+mv frontEnd/holdTSF3.txt holdTSF3.txt
 mv frontEnd/VAL.txt ${VAL}.txt
 
 
-sed '$d' < TSF1.txt > hold ; mv hold TSF1.txt
-cat TSF1.txt > TSF.txt
-sed '$d' < TSF2.txt > hold ; mv hold TSF2.txt
-cat TSF2.txt >> TSF.txt
-cat TSF3.txt >> TSF.txt
-cat TSF.txt >| "${nTS}"
+sed '$d' < holdTSF1.txt > hold ; mv hold holdTSF1.txt
+cat holdTSF1.txt > holdTSF.txt
+sed '$d' < holdTSF2.txt > hold ; mv hold holdTSF2.txt
+cat holdTSF2.txt >> holdTSF.txt
+cat holdTSF3.txt >> holdTSF.txt
+cp holdTSF.txt "${nTS}"
 
 #back office
 
 mv $MAF backEnd/MAF.txt
-mv TSF.txt backEnd/TSF.txt
+mv holdTSF.txt backEnd/TSF.txt
 
 cd backEnd
 java BackEnd MAF.txt TSF.txt nMAF.txt nVAL.txt
 cd ../
 
 mv backEnd/MAF.txt $MAF
-mv backEnd/TSF.txt TSF.txt
+mv backEnd/TSF.txt holdTSF.txt
 mv backEnd/nMAF.txt $nMAF
 mv backEnd/nVAL.txt $nVAL
 
-rm TSF.txt
-rm TSF1.txt
-rm TSF2.txt
-rm TSF3.txt
+rm holdTSF.txt
+rm holdTSF1.txt
+rm holdTSF2.txt
+rm holdTSF3.txt
