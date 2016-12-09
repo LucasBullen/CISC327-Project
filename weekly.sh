678c4dd34e062c49ed5e36c05418dfc934182fb6
Daniel2218 <daniel_lucia22@hotmail.com>
2016-12-08 21:01:55 -0500

    Do not use swear words on the git grant. Added echos

--- weekly.sh
+++ weekly.sh
@@ -1,5 +1,3 @@
-#chmod u+x Daily.sh
-
 >| MAF.txt
 >| VAL.txt
 
@@ -8,6 +6,8 @@ do
     TS1="day"$i"TS1.txt"
     TS2="day"$i"TS2.txt"
     TS3="day"$i"TS3.txt"
+
+    echo "Day "$i
 
     # ./Daily.sh VAL day1TS1.txt  day1TS2.txt day1TS3.txt MAF.txt nMAF.txt nVAL.txt
     ./Daily.sh VAL $TS1 $TS2 $TS3 MAF.txt nMAF${i}.txt nVAL${i}.txt TSF${i}.txt
